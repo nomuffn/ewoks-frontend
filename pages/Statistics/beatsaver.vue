cd55f74de9308e8733d22b1328f9e8d0549e9d2a
nomuffn <robin.rs80@gmail.com>
2021-08-30 12:12:32 +0200

    shiny

--- pages/Statistics/beatsaver.vue
+++ pages/Statistics/beatsaver.vue
@@ -1,31 +1,36 @@
 <template>
     <div class="beatsaver">
-        <div class="filter">
-            <h2>Filter</h2>
-            <vs-select placeholder="Order by" v-model="column">
-                <vs-option v-for="col in allColumns" :key="col" :label="col" :value="col">
-                    {{ col }}
-                </vs-option>
-            </vs-select>
-            <p>
-                <strong>Minimum stars: {{ stars }}</strong>
-            </p>
-            <slider v-model="stars" :max="14" :tooltips="false" />
-            <p>
-                <strong>Minimum bpm: {{ bpm }}</strong>
-            </p>
-            <slider v-model="bpm" :max="350" :tooltips="false" />
-            <div class="radios">
-                <vs-radio v-model="descending" val="-">
-                    Descending
-                </vs-radio>
-                <vs-radio style="margin-left: 10px" v-model="descending" val="">
-                    Ascending
-                </vs-radio>
+        <div class="filter-container">
+            <div class="filter">
+                <h2>Filter</h2>
+                <vs-select placeholder="Order by" v-model="column">
+                    <vs-option v-for="col in allColumns" :key="col" :label="col" :value="col">
+                        {{ col }}
+                    </vs-option>
+                </vs-select>
+                <p>
+                    <strong>Minimum stars: {{ stars }}</strong>
+                </p>
+                <slider v-model="stars" :max="14" :tooltips="false" />
+                <p>
+                    <strong>Minimum bpm: {{ bpm }}</strong>
+                </p>
+                <slider v-model="bpm" :max="350" :tooltips="false" />
+                <div class="radios">
+                    <vs-radio v-model="descending" val="-">
+                        Descending
+                    </vs-radio>
+                    <vs-radio style="margin-left: 10px" v-model="descending" val="">
+                        Ascending
+                    </vs-radio>
+                </div>
+                <vs-button :loading="loading" @click="loadResults">
+                    Filter
+                </vs-button>
+                <vs-alert color="primary">
+                    Beatsaver doesn't give downloads properly anymore :(
+                </vs-alert>
             </div>
-            <vs-button :loading="loading" @click="loadResults">
-                Filter
-            </vs-button>
         </div>
         <div class="results-container">
             <h2>Results</h2>
@@ -35,17 +40,17 @@
                         <div class="top">
                             <div>
                                 <p class="mapper">{{ item["levelAuthorName"] }}</p>
-                                <p class="name">{{ item["songName"] }}</p>
+                                <h3 class="name">{{ item["songName"] }}</h3>
                             </div>
                             <div>
                                 <p>#{{ index + 1 }}</p>
                             </div>
                         </div>
                         <div class="stats">
-                            <p :class="{ bold: isBold('downloads') }">Downloads: {{ item["downloads"] }}</p>
-                            <p :class="{ bold: isBold('upvotes') }">Upvotes: {{ item["upvotes"] }}</p>
-                            <p :class="{ bold: isBold('downvotes') }">Downvotes: {{ item["downvotes"] }}</p>
-                            <p :class="{ bold: isBold('ratio') }">Ratio: {{ item["ratio"] }}</p>
+                            <p :class="{ colored: isActive('downloads') }">Downloads: {{ item["downloads"] }}</p>
+                            <p :class="{ colored: isActive('upvotes') }">Upvotes: {{ item["upvotes"] }}</p>
+                            <p :class="{ colored: isActive('downvotes') }">Downvotes: {{ item["downvotes"] }}</p>
+                            <p :class="{ colored: isActive('ratio') }">Ratio: {{ item["ratio"] }}</p>
                             <p>Star: {{ item["highestStar"] }}</p>
                         </div>
                     </template>
@@ -68,7 +73,7 @@ export default {
             descending: "-",
             stars: 0,
             bpm: 0,
-            allColumns: ["Ratio", "Downloads", "Upvotes", "Downvotes"],
+            allColumns: ["Ratio", "Upvotes", "Downvotes"],
             column: "Ratio",
             results: [],
             visibleItems: 10
@@ -85,9 +90,10 @@ export default {
             this.results = [];
             const data = { column: this.descending + this.column.toLowerCase(), stars: this.stars, bpm: this.bpm };
             this.results = await this.$defaultApi.$post("beatsaver/stats", data);
+            this.visibleItems = 10;
             this.loading = false;
         },
-        isBold(string) {
+        isActive(string) {
             return this.column.toLowerCase() == string;
         }
     }
@@ -96,8 +102,6 @@ export default {
 
 <style lang="scss">
 .page .beatsaver {
-    display: flex;
-    place-content: center;
     margin-top: 20px;
 
     h2 {
@@ -106,39 +110,46 @@ export default {
         margin-bottom: 10px;
     }
 
-    .filter {
+    .filter-container {
         flex: 1;
-        max-width: 300px;
-        margin-right: 200px;
+        display: flex;
+        justify-content: center;
 
-        .radios {
-            display: flex;
-            margin-top: 20px;
-        }
+        .filter {
+            max-width: 400px;
 
-        > p {
-            color: #fff;
-            margin-top: 15px;
-        }
+            .radios {
+                display: flex;
+                margin-top: 20px;
+            }
 
-        .slider {
-            margin-top: 10px;
-        }
-        .vs-select-content {
-            margin-top: 10px;
-            max-width: inherit;
-        }
+            > p {
+                color: #fff;
+                margin-top: 15px;
+            }
 
-        .vs-button {
-            width: 100%;
-            margin-top: 20px;
+            .slider {
+                margin-top: 10px;
+                padding-left: 10px;
+            }
+            .vs-select-content {
+                margin-top: 10px;
+                max-width: inherit;
+            }
+
+            .vs-button {
+                width: 100%;
+                margin: 20px 0px;
+            }
         }
     }
     .results-container {
         flex: 1;
 
-        h3 {
-            text-align: center;
+        .results {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
         }
 
         .vs-card-content {
@@ -155,7 +166,6 @@ export default {
                     .mapper {
                     }
                     .name {
-                        font-weight: bold;
                     }
                 }
                 .stats {
@@ -164,15 +174,11 @@ export default {
                     place-content: space-between;
                     > p {
                         white-space: nowrap;
-                        margin-right: 25px;
+                        width: 150px;
                     }
                 }
             }
         }
-    }
-
-    .bold {
-        font-weight: bold;
     }
 }
 </style>
