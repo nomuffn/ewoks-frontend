0b53af243126c5cab215cf1ebc39dfb4ed636a57
nomuffn <robin.rs80@gmail.com>
2022-04-23 20:04:44 +0200

    beatsaver stats tracker

--- plugins/global-components.js
+++ plugins/global-components.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Slider from '@vueform/slider/dist/slider.vue2.js'
 import Loading from '@/components/LoadingSpinner.vue'
+import VueGoodTablePlugin from 'vue-good-table';
+
+import 'vue-good-table/dist/vue-good-table.css'
 
 Vue.component('Slider', Slider)
 Vue.component('Loading', Loading)
+Vue.use(VueGoodTablePlugin);
